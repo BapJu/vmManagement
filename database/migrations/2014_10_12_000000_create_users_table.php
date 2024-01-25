8d692bc71c695dbd81c91ddcdd1f259190654160
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-01-25 16:18:29 +0100

    init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 20); // Longueur ajustée pour correspondre à votre script SQL
+            $table->string('email', 20)->unique(); // Utilisez soit 'email' soit 'mail'
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 15); // Longueur ajustée
+            $table->unsignedBigInteger('id_role'); // Ajout du champ id_role
             $table->rememberToken();
             $table->timestamps();
+
+            // Définition de la clé étrangère pour id_role
+            $table->foreign('id_role')->references('id')->on('roles');
         });
     }
 
@@ -30,3 +34,4 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
