8e7122c1777787055ea3adcbf076c3c1bfc2f937
BuildTools <unconfigured@null.spigotmc.org>
2024-03-08 10:13:29 +0100

    stop logique v.1.15-fonctionnel

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -142,7 +142,7 @@ class EventController extends Controller
             $command = "sudo ansible-playbook " . base_path('/scripts/stop_containers.yml');
             $output = shell_exec($command);
             echo $output;
-            $event->active = true;
+            $event->active = false;
             $event->save();
             return response()->json(['message' => 'Event stopped successfully']);
         } else {
