5a518563e7be91b11b0030bdc522f4566f52eeed
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-08 14:46:17 +0100

    delete remember token

--- database/migrations/2024_01_30_130018_create_user_table.php
+++ database/migrations/2024_01_30_130018_create_user_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
                 $table->unsignedBigInteger('id_role')->nullable();
-                $table->rememberToken();
                 $table->timestamps();
 
                 // Définition de la clé étrangère pour id_role
