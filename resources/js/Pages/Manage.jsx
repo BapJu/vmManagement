e9d271e70a1fd7546f0347a35e4036bb2cea2549
BuildTools <unconfigured@null.spigotmc.org>
2024-03-08 11:12:32 +0100

    stop logique v.1.19-fonctionnel

--- resources/js/Pages/Manage.jsx
+++ resources/js/Pages/Manage.jsx
@@ -78,7 +78,7 @@ export default function Manage({ auth }) {
 
             .then(response => response.json())
             .then(data => {
-                console.log('Success:', data);
+                //console.log('Success:', data);
                 // Vous pouvez ajouter ici du code pour mettre à jour l'interface utilisateur en fonction de la réponse
             })
             .catch((error) => {
