c12e766f7f091285c701dc5d10c999fba8bfaa2a
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-11 15:26:12 +0100

    ajout name vm

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -255,7 +255,7 @@ export default function Dashboard({auth}) {
                                                 <input
                                                     type="text"
                                                     value="-XXXXX"
-                                                    className="mt-1 block w-1/6 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                    className="mt-1 block w-1/4 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                                     disabled
                                                 />
                                             </div>
