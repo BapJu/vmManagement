37a6d10039a2fbd97ab9d4e6355dbf9d27843a5f
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-11 14:04:11 +0100

    add footer

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -132,123 +132,129 @@ export default function Dashboard({auth}) {
 
     console.log(data);
     return (<AuthenticatedLayout
-        user={auth.user}
-        header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
 
-    >
+        >
 
-        <Head title="Dashboard"/>
+            <Head title="Dashboard"/>
 
-        <div className="py-2">
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                {/* VM Statistics Dashboard */}
-                <div className="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div className="p-6 text-gray-900">
-                        <h3 className="font-semibold text-lg">Vos Statistics VM</h3>
-                        <div>Total VMs Created: {vmStats.totalCreated}</div>
-                        <div>Total VMs Active: {vmStats.totalActive}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="ax-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div className="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div className="max-w-lg mx-auto mt-4">
-                    <h3 className="font-semibold text-lg">Créer une VM</h3>
-                    <div className="flex justify-center items-center" style={{minHeight: '500px'}}>
-                        {isLoading ? (<div
-                            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                            role="status">
-                                <span
-                                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                                >Loading...</span>
-                        </div>) : (<form onSubmit={handleSubmit}>
-                            <div className="mb-4">
-                                <label htmlFor="localisation" className="block text-gray-700">Site:</label>
-                                <select
-                                    id="localisation"
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    onChange={(e) => setData('id_localisation', e.target.value)}
-                                    required
-                                >
-                                    {localisations.map(localisation => (<option key={localisation.id}
-                                                                                value={localisation.id}>{localisation.name}</option>))}
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="subject" className="block text-gray-700">Domaine:</label>
-                                <select
-                                    id="subject"
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    onChange={(e) => setData('id_subject', e.target.value)}
-                                    required
-                                >
-                                    {subjects.map(subject => (<option key={subject.id}
-                                                                      value={subject.id}>{subject.description}</option>))}
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="storage" className="block text-gray-700">Mémoire:</label>
-                                <select
-                                    id="storage"
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    onChange={(e) => setData('id_storage', e.target.value)}
-                                    required
-                                >
-                                    {storages.map(storage => (
-                                        <option key={storage.id} value={storage.id}>{storage.name}</option>))}
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="template" className="block text-gray-700">Template:</label>
-                                <select
-                                    id="template"
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    onChange={(e) => setData('id_typeofvm', e.target.value)}
-                                    required
-                                >
-                                    <option value="">Choisissez votre template</option>
-                                    {templates.map(template => (<option key={template.id}
-                                                                        value={template.id}>{template.description}</option>))}
-                                </select>
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="end_date" className="block text-gray-700">Date de fin des
-                                    Vms:</label>
-                                <input
-                                    id="end_date"
-                                    type="date"
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    onChange={(e) => setData('end_date', e.target.value)}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="vmCount" className="block text-gray-700">Nombre de VM à
-                                    créer:</label>
-                                <input
-                                    id="vmCount"
-                                    type="number"
-                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                    value={vmCount}
-                                    onChange={handleVmCountChange} // Utilisez handleVmCountChange pour gérer les changements
-                                    required
-                                />
-
-                            </div>
-                            <button
-                                type="submit"
-                                className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                                disabled={isLoading}
-                            >
-                                Créer VM
-                            </button>
-                            {isLoading && <div>Loading...</div>}
-                        </form>)}
+            <div className="py-2">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {/* VM Statistics Dashboard */}
+                    <div className="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <h3 className="font-semibold text-lg">Vos Statistics VM</h3>
+                            <div>Total VMs Created: {vmStats.totalCreated}</div>
+                            <div>Total VMs Active: {vmStats.totalActive}</div>
                         </div>
                     </div>
                 </div>
             </div>
-    </AuthenticatedLayout>
+            <div className="ax-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="mt-8 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div className="max-w-lg mx-auto mt-4">
+                        <h3 className="font-semibold text-lg">Créer une VM</h3>
+                        <div className="flex justify-center items-center" style={{minHeight: '500px'}}>
+                            {isLoading ? (<div
+                                className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                                role="status">
+                                <span
+                                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                                >Loading...</span>
+                            </div>) : (<form onSubmit={handleSubmit}>
+                                <div className="mb-4">
+                                    <label htmlFor="localisation" className="block text-gray-700">Site:</label>
+                                    <select
+                                        id="localisation"
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        onChange={(e) => setData('id_localisation', e.target.value)}
+                                        required
+                                    >
+                                        {localisations.map(localisation => (<option key={localisation.id}
+                                                                                    value={localisation.id}>{localisation.name}</option>))}
+                                    </select>
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="subject" className="block text-gray-700">Domaine:</label>
+                                    <select
+                                        id="subject"
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        onChange={(e) => setData('id_subject', e.target.value)}
+                                        required
+                                    >
+                                        {subjects.map(subject => (<option key={subject.id}
+                                                                          value={subject.id}>{subject.description}</option>))}
+                                    </select>
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="storage" className="block text-gray-700">Mémoire:</label>
+                                    <select
+                                        id="storage"
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        onChange={(e) => setData('id_storage', e.target.value)}
+                                        required
+                                    >
+                                        {storages.map(storage => (
+                                            <option key={storage.id} value={storage.id}>{storage.name}</option>))}
+                                    </select>
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="template" className="block text-gray-700">Template:</label>
+                                    <select
+                                        id="template"
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        onChange={(e) => setData('id_typeofvm', e.target.value)}
+                                        required
+                                    >
+                                        <option value="">Choisissez votre template</option>
+                                        {templates.map(template => (<option key={template.id}
+                                                                            value={template.id}>{template.description}</option>))}
+                                    </select>
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="end_date" className="block text-gray-700">Date de fin des
+                                        Vms:</label>
+                                    <input
+                                        id="end_date"
+                                        type="date"
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        onChange={(e) => setData('end_date', e.target.value)}
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="vmCount" className="block text-gray-700">Nombre de VM à
+                                        créer:</label>
+                                    <input
+                                        id="vmCount"
+                                        type="number"
+                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                        value={vmCount}
+                                        onChange={handleVmCountChange} // Utilisez handleVmCountChange pour gérer les changements
+                                        required
+                                    />
 
-);
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    disabled={isLoading}
+                                >
+                                    Créer VM
+                                </button>
+                                {isLoading && <div>Loading...</div>}
+                            </form>)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer className="py-6 bg-gray-100 dark:bg-gray-800">
+                <div className="text-center text-sm text-gray-500 dark:text-gray-400">
+                    Created by Baptiste & Alexis - Projet M1 2024
+                </div>
+            </footer>
+        </AuthenticatedLayout>
+
+
+    );
 }
