235c6287b72b11364e5fe1ad0c26b63a7de17693
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-11 11:21:16 +0100

    redrirect after sumbit

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head, useForm} from '@inertiajs/react';
 import {useEffect, useState} from 'react';
+import { router } from '@inertiajs/vue3'
 
 export default function Dashboard({auth}) {
     const [vmStats, setVmStats] = useState({totalCreated: 0, totalActive: 0});
