c42c5e6248a0fb8c0fd29ccd145deebf93b8580d
BuildTools <unconfigured@null.spigotmc.org>
2024-03-11 16:59:45 +0100

    lancement des VMs a la creation - v1.2

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -78,19 +78,17 @@ class EventController extends Controller
 
         $vmIDStart = DB::table('event')->max('vmid') + 1;
 
-        $firstVmid = null;
-        $lastVmid = null;
-
         foreach ($ipAvailable as $index => $ip) {
             $vmid = $vmIDStart + $index;
-
-            if ($index === 0) {
-                // Stockez la première vmid
-                $firstVmid = $vmid;
-            }
-
-            // ... (restez avec le reste du code)
-
+            $dataForYAML[] = [
+                'template_vmid' => $templateVMID,
+                'vmid' => str_replace('.', '', $ip->available_ip),
+                'static_ip' => $ip->available_ip,
+                'gateway' => "10.{$mask_site}.{$mask_subject}.1",
+                'cloneName' => $category,
+                'storage' => $storage,
+                'resource_pool' => 'Serveurs',
+            ];
             $event = new Event();
             $event->id_typeofvm = $typeOfVm;
             $event->id_user = $request->input('id_user');
@@ -100,21 +98,15 @@ class EventController extends Controller
             $event->ip = $ip->available_ip;
             $event->active = true;
             $event->save();
-
-            // Stockez la dernière vmid à chaque itération
-            $lastVmid = $vmid;
         }
-
 
         $yamlContent = YAMLGenerator::generateYAML($dataForYAML);
         file_put_contents(base_path('/scripts/clones.yml'), $yamlContent);
 
 
+
         $command = "sudo ansible-playbook " . base_path('/scripts/clone_configure_lxc.yml');
         exec($command);
-
-        $commandExec = "sudo ansible-playbook " . base_path('/scripts/start_containers.yml') . " --extra-vars 'param_start_vmid={$firstVmid} param_end_vmid={$lastVmid}'";
-        exec($commandExec);
 
         return redirect('/manage');
     }
