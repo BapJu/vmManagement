f5d359fd1c55fbbeaf598f10393ecb1202ee0584
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-11 10:16:15 +0100

    correct input nb

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -223,7 +223,6 @@ export default function Dashboard({auth}) {
                                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                                     value={vmCount}
                                     onChange={(e) => setData('nb_vm', e.target.value)}
-                                    min="1"
                                     required
                                 />
                             </div>
