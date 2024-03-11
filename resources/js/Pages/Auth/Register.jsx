5154707d27916e603d61f201399fd13018ec2f82
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-11 16:07:07 +0100

    add localisation to register form

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
@@ -9,10 +9,24 @@ import { Head, Link, useForm } from '@inertiajs/react';
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
         name: '',
+        id_localisation: '',
         email: '',
         password: '',
         password_confirmation: '',
     });
+
+    const [localisations, setLocalisation] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/localisations')
+            .then(response => response.json())
+            .then(data => {
+                setLocalisation(data);
+            })
+            .catch(error => {
+                console.error('Error fetching roles:', error);
+            });
+    }, []);
 
     useEffect(() => {
         return () => {
@@ -26,13 +40,14 @@ export default function Register() {
         post(route('register'));
     };
 
+
     return (
         <GuestLayout>
             <Head title="Register" />
 
             <form onSubmit={submit}>
                 <div>
-                    <InputLabel htmlFor="name" value="Name" />
+                    <InputLabel htmlFor="name" value="Full Name"/>
 
                     <TextInput
                         id="name"
@@ -42,14 +57,28 @@ export default function Register() {
                         autoComplete="name"
                         isFocused={true}
                         onChange={(e) => setData('name', e.target.value)}
+                        placeholder="John Doe"
                         required
                     />
 
-                    <InputError message={errors.name} className="mt-2" />
+                    <InputError message={errors.name} className="mt-2"/>
+                </div>
+
+                <div className="mb-4">
+                    <label htmlFor="localisation" className="block text-gray-700">Site:</label>
+                    <select
+                        id="localisation"
+                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                        onChange={(e) => setData('id_localisation', e.target.value)}
+                        required
+                    >
+                        {localisations.map(localisation => (<option key={localisation.id}
+                                                                    value={localisation.id}>{localisation.name}</option>))}
+                    </select>
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
+                    <InputLabel htmlFor="email" value="Email"/>
 
                     <TextInput
                         id="email"
@@ -62,11 +91,11 @@ export default function Register() {
                         required
                     />
 
-                    <InputError message={errors.email} className="mt-2" />
+                    <InputError message={errors.email} className="mt-2"/>
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                    <InputLabel htmlFor="password" value="Password"/>
 
                     <TextInput
                         id="password"
@@ -79,11 +108,11 @@ export default function Register() {
                         required
                     />
 
-                    <InputError message={errors.password} className="mt-2" />
+                    <InputError message={errors.password} className="mt-2"/>
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+                    <InputLabel htmlFor="password_confirmation" value="Confirm Password"/>
 
                     <TextInput
                         id="password_confirmation"
@@ -96,7 +125,7 @@ export default function Register() {
                         required
                     />
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
+                    <InputError message={errors.password_confirmation} className="mt-2"/>
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
