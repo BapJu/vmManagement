1c1b8b84f0db0f5713fc8b73fdfb575c23cc8408
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-13 10:51:24 +0100

    add name field

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -250,7 +250,7 @@ export default function Dashboard({ auth }) {
                                                 id="vmCount"
                                                 type="number"
                                                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                                value={vmCount}
+                                                value={data.nb_vm}
                                                 onChange={handleVmCountChange}
                                                 required
                                             />
