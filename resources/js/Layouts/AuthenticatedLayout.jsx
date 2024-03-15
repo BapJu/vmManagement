f448e2acd709857eaa98feb53c228a0aa5833939
BuildTools <unconfigured@null.spigotmc.org>
2024-03-15 15:10:04 +0100

    admin color

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -8,26 +8,32 @@ import { Link } from '@inertiajs/react';
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
+    const authLayoutClass = user.id_role === 1 ? 'bg-admin-color' : 'bg-gray-100';
+
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className={`min-h-screen ${authLayoutClass}`}>
             <nav className="bg-white border-b border-gray-100">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800"/>
                                 </Link>
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>Dashboard</NavLink>
-                                <NavLink href={route('manage')} active={route().current('manage')}>Gérer les Vms</NavLink>
-                                {user.id_role ===1 &&(
-                                <NavLink href={route('statistiques')} active={route().current('statistiques')}>Statistiques</NavLink>
+                                <NavLink href={route('dashboard')}
+                                         active={route().current('dashboard')}>Dashboard</NavLink>
+                                <NavLink href={route('manage')} active={route().current('manage')}>Gérer les
+                                    Vms</NavLink>
+                                {user.id_role === 1 && (
+                                    <NavLink href={route('statistiques')}
+                                             active={route().current('statistiques')}>Statistiques</NavLink>
                                 )}
-                                {user.id_role ===1 &&(
-                                    <NavLink href={route('manage-users')} active={route().current('manage-users')}>Gestion des utilisateurs</NavLink>
+                                {user.id_role === 1 && (
+                                    <NavLink href={route('manage-users')} active={route().current('manage-users')}>Gestion
+                                        des utilisateurs</NavLink>
                                 )}
                             </div>
                         </div>
