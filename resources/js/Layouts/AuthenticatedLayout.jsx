8231924690d3f4a0ecdd05cfe10f4523e9cbfd4a
BuildTools <unconfigured@null.spigotmc.org>
2024-03-15 15:24:19 +0100

    admin color

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -13,8 +13,8 @@ export default function Authenticated({ user, header, children }) {
     return (
         <div className="min-h-screen bg-gray-100">
             <nav className="bg-white border-b border-gray-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className={`flex justify-between h-16 ${navLinksClass}`}>
+                <div className={`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ${navLinksClass}`}>
+                    <div className="flex justify-between h-16">
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
