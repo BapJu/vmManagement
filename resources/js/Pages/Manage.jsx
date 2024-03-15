1aac72d95728124f2921e234c7d7dd2d1928862d
BuildTools <unconfigured@null.spigotmc.org>
2024-03-15 12:10:51 +0100

    probleme affichage - v4.1

--- resources/js/Pages/Manage.jsx
+++ resources/js/Pages/Manage.jsx
@@ -272,8 +272,11 @@ export default function Manage({ auth }) {
                             <tbody className="bg-white divide-y divide-gray-200">
                             {events.map(event => (
                                 (historiqueChecked || event.ip !== null) && // Afficher si l'historique est coché ou s'il y a une adresse IP
-                                ((auth.user.id_role === 1) || (auth.user.id === selectedUserId && event.id_user === auth.user.id)) && (
-                                    // Si l'utilisateur est un administrateur ou si l'utilisateur non administrateur est sélectionné et l'événement appartient à cet utilisateur
+                                (
+                                    (auth.user.id_role === 1) || // Si l'utilisateur est un administrateur
+                                    (selectedUserId && event.id_user === selectedUserId) || // Si un utilisateur est sélectionné et l'événement appartient à cet utilisateur
+                                    (!selectedUserId && event.id_user === auth.user.id) // Si aucun utilisateur n'est sélectionné et l'événement appartient à l'utilisateur actuel
+                                ) && (
                                     <tr key={event.id}>
                                         <td className="px-6 py-4 whitespace-nowrap">{event.namevm}</td>
                                         <td className="px-6 py-4 whitespace-nowrap">
@@ -283,17 +286,21 @@ export default function Manage({ auth }) {
                                         {event.ip !== null && (
                                             <td className="px-6 py-4 whitespace-nowrap">
                                                 {event.active ? (
-                                                    <FontAwesomeIcon icon={faStop} onClick={() => handleStopVM(event.id)} className="cursor-pointer mr-2"/>
+                                                    <FontAwesomeIcon icon={faStop}
+                                                                     onClick={() => handleStopVM(event.id)}
+                                                                     className="cursor-pointer mr-2"/>
                                                 ) : (
-                                                    <FontAwesomeIcon icon={faPlay} onClick={() => handleStartVM(event.id)} className="cursor-pointer mr-2"/>
+                                                    <FontAwesomeIcon icon={faPlay}
+                                                                     onClick={() => handleStartVM(event.id)}
+                                                                     className="cursor-pointer mr-2"/>
                                                 )}
-                                                <FontAwesomeIcon icon={faTrash} onClick={() => handleDeleteVM(event.id)} className="cursor-pointer"/>
+                                                <FontAwesomeIcon icon={faTrash} onClick={() => handleDeleteVM(event.id)}
+                                                                 className="cursor-pointer"/>
                                             </td>
                                         )}
                                     </tr>
                                 )
                             ))}
-
                             </tbody>
 
 
