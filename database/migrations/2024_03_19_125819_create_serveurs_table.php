28dc7d1f4ae6b76a5433ccb4b09c9b37b255b761
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-19 14:09:50 +0100

    add server table

---
+++ database/migrations/2024_03_19_125819_create_serveurs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('serveur', function (Blueprint $table) {
+            $table->id();
+            $table->string('adress_ip');
+            $table->string('noeud');
+            $table->string('ssh_user');
+            $table->string('ssh_password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('serveur');
+    }
+};
