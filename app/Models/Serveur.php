dd08e8bc4defb71fbdc3f525fbf17380f5b342f4
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-19 14:44:35 +0100

    add serveur model

---
+++ app/Models/Serveur.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Serveur extends Model
+{
+    protected $table = 'serveur';
+    use HasFactory;
+}
