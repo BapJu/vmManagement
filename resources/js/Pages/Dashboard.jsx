302208b8a308b95ed39289a6cf8e3e1987e04973
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-03-20 10:06:33 +0100

    filtre serveurs

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -222,7 +222,7 @@ export default function Dashboard({ auth }) {
                                             </select>
                                         </div>
                                         <div className="mb-4">
-                                            <label htmlFor="serveur" className="block text-gray-700">Mémoire:</label>
+                                            <label htmlFor="serveur" className="block text-gray-700">Serveurs:</label>
                                             <select
                                                 id="serveur"
                                                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
